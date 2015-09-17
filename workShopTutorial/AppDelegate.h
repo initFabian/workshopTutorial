6d5b51a47d3b1d8ca59f2cc23f81cb3f78a97160
Fabian Buentello <fbuentello@mioty.com>
2015-09-17 04:19:52 -0500

    Initial Commit

---
+++ workShopTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  workShopTutorial
+//
+//  Created by Fabian Buentello on 9/17/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ workShopTutorial/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  workShopTutorial
+//
+//  Created by Fabian Buentello on 9/17/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
